eda7ddc8f24652a4f5e4ecbf23f8346f42119bd6
Markus Holopainen <markus.x.holopainen@helsinki.fi>
2022-05-27 16:49:34 +0300

    e3.16

--- index.js
+++ index.js
@@ -25,17 +25,24 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
     })
   })
 
-  app.get('/api/persons', (req, res) => {
+  app.get('/api/persons', (req, res, next) => {
     Person.find({}).then(persons => {
       res.json(persons)
     })
+    .catch(error => next(error))
   })
 
-  app.get('/api/persons/:id', (req, res) => {
+  app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id).then(person => {
-      console.log(person)
-      res.json(person)
+      console.log('person', person)
+      if (person){
+        console.log(person)
+        res.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
 
   app.post('/api/persons', (req, res) => {
@@ -52,15 +59,33 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
       person.save().then(savedPerson => {
         res.status(201).json(person)
       })
+      .catch(error => next(error))
     }
   })
 
-  app.delete('/api/persons/:id', (req, res) => {
+  app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
       .then(result => {
         res.status(204).end()
       })
+      .catch(error => next(error))
   })
+
+  const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+  }
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+  }
+  
+  app.use(errorHandler)
 
   const PORT = process.env.PORT
   app.listen(PORT, () => {
