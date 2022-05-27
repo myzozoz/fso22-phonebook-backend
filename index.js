e214a005649afba25b0212bbe7e8647e50932c56
Markus Holopainen <markus.x.holopainen@helsinki.fi>
2022-05-27 16:25:21 +0300

    e3.13-3.14

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -14,63 +17,40 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
-let persons = [
-    {
-      name: "Arto Hellas",
-      number: "040-1234567",
-      id: 1
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4
-    }
-  ]
-
   app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    Person.find({}).then(persons => {
+      res.send(`<p>Phonebook has info for ${persons.length} people</p>
       <p>${new Date()}</p>`)
+    })
   })
 
   app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+      res.json(persons)
+    })
   })
 
   app.get('/api/persons/:id', (req, res) => {
-    const person = persons.find(p => p.id === Number(req.params.id))
-    console.log(person)
-
-    if (!person) {
-      res.status(404).end()
-    } else (
+    Person.findById(req.params.id).then(person => {
+      console.log(person)
       res.json(person)
-    )
+    })
   })
 
   app.post('/api/persons', (req, res) => {
-    const person = {...req.body}
-    person.id = Math.floor(Math.random()*999999)
+    const body = {...req.body}
     
-    if (!person.name || !person.number) {
-      res.json({error: 'Name or number missing'})
-      res.status(400)
-    } else if (persons.findIndex(p => p.name === person.name) > -1) {
-      res.json({error: 'Name already exists'})
-      res.status(400)
+    if (!body.name || !body.number) {
+      res.status(400).json({error: 'Name or number missing'})
     } else {
-      persons = persons.concat(person)
-      res.json(person)
-      res.status(201)
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+
+      person.save().then(savedPerson => {
+        res.status(201).json(person)
+      })
     }
   })
 
@@ -79,7 +59,7 @@ let persons = [
     res.status(204).end()
   })
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
