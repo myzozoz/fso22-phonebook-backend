8a83492460833543b08411128dff4ab494495670
Markus Holopainen <markus.x.holopainen@helsinki.fi>
2022-05-27 13:02:07 +0300

    e3.2-3.3

--- index.js
+++ index.js
@@ -24,10 +24,27 @@ let persons = [
     }
   ]
 
+  app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`)
+  })
 
   app.get('/persons', (req, res) => {
     res.json(persons)
   })
+
+  app.get('/persons/:id', (req, res) => {
+    const person = persons.find(p => p.id === Number(req.params.id))
+    console.log(person)
+
+    if (!person) {
+      res.status(404).end()
+    } else (
+      res.json(person)
+    )
+  })
+
+ 
 
   const PORT = 3001
   app.listen(PORT, () => {
